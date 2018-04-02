00c7a0e1fdd2e0d743071aaee9a3dcfab4bd56ca
Sneaky657 <36302885+Sneaky657@users.noreply.github.com>
2018-04-01 20:23:48 -0400

    added death animations and credit scene

--- Scripts/Playerhealth.cs
+++ Scripts/Playerhealth.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Playerhealth : MonoBehaviour {
+public class Playerhealth : MonoBehaviour
+{
     public int lives = 3;
+    public GameObject DeathAnim;
     public int health = 15; // player health. default is set to 15
     public bool isDead = false;     // used to tell if the player has died
     private Vector3 Startposition;  // used to respawn the player
@@ -11,9 +13,10 @@ public class Playerhealth : MonoBehaviour {
     public GameObject[] checkpoint;
     private GameObject[] Hazards;  // array for hazards
     private Damageplayer Damage;   // stores the current hazards damage
-    
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         Hazards = GameObject.FindGameObjectsWithTag("Hazards");  // get all the hazards in the map
         Startposition.x = transform.position.x;
         Startposition.y = transform.position.y;         // get original start position
@@ -27,7 +30,7 @@ public class Playerhealth : MonoBehaviour {
             if (collision.gameObject == checkpoint[i].gameObject)  // used to determine which checkpoint the player is at
             {                                                        // currently doesnt work at all
                 Startposition = checkpoint[i].transform.position;
-                
+
                 checkpoint[i].active = false;
             }
         }
@@ -36,33 +39,41 @@ public class Playerhealth : MonoBehaviour {
         {
             if (collision.gameObject == Hazards[i].gameObject) // check if the player has collided with one of the hazards
             {
-                
-                    Damage = Hazards[i].GetComponent<Damageplayer>();  // get the current hazards damage 
 
-                    if (Damage.Damage >= health) // check if the hazard has one shot the player else just subtract the damage from health
-                    {
+                Damage = Hazards[i].GetComponent<Damageplayer>();  // get the current hazards damage 
+
+                if (Damage.Damage >= health) // check if the hazard has one shot the player else just subtract the damage from health
+                {
                     lives--;
-                        isDead = true;
-                    } 
-                    else
-                    {
-                        health = health - Damage.Damage;    // subtract health                    
+                    isDead = true;
+                }
+                else
+                {
+                    health = health - Damage.Damage;    // subtract health                    
 
-                    }
-                
+                }
+
             }
         }
     }
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
 
 
-		if (isDead == true)
+        if (isDead == true)
         {
             isDead = false;
-            
+            GameObject death = Instantiate<GameObject>(DeathAnim);
+            death.transform.position = Startposition;
             transform.position = Startposition;  // if the player dies set them back at the beginning
+            if (lives == 0)
+            {
+
+                    UnityEngine.SceneManagement.SceneManager.LoadScene("CreditScreen");
+            }
+
         }
-        
+
     }
 }
